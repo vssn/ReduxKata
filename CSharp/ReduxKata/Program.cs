77993d874439a3797dfa45f9cd952c122ae34a01
vssn <matrim.eg@gmail.com>
2018-06-29 20:43:01 +0200

    Added c# redux

---
+++ CSharp/ReduxKata/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ReduxKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, string, int> counter = delegate(int state, string action) {
+                return state;
+            };
+            
+            var store = Store.createStore(counter);
+            Console.Write(store.getState());
+        }
+
+        
+
+    }
+
+    class Store {
+        private int state;
+        private Func<int, string, int> reducer;
+        public int getState() {
+            return this.state;
+        }
+        public static Store createStore(Func<int, string, int> reducer) {
+            return new Store(reducer);
+        }
+
+        Store(Func<int, string, int> reducer) {
+            this.reducer = reducer;
+            this.state = 0;
+        }
+
+
+    }
+
+    class Reducer {
+
+    }
+}
