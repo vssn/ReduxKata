d17c534c6eda0e181696ab39634071373751292e
Edmund <matrim.eg@gmail.com>
2019-10-01 18:58:49 +0200

    Updated store with Listener type for function

--- Java/src/com/company/Store.java
+++ Java/src/com/company/Store.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.List;
 import java.util.Vector;
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -7,27 +8,41 @@ import java.util.function.Function;
 public class Store{
     private int state;
     private BiFunction<Integer, Action, Integer> reducer;
-    private Vector<Function<Integer, Integer>> listeners;
+    private Vector<Listener> listeners;
 
     private Store(BiFunction<Integer, Action, Integer> reducer) {
         this.reducer = reducer;
-        this.listeners = new Vector<Function<Integer, Integer>>();
+        this.listeners = new Vector<Listener>();
         this.state = 0;
     }
 
-    public static Store createStore(BiFunction<Integer, Action, Integer> reducer) {
+    static Store createStore(BiFunction<Integer, Action, Integer> reducer) {
         return new Store(reducer);
     }
 
     public void subscribe(Function<Integer, Integer> listener) {
-        this.listeners.add(listener);
+
+        this.listeners.add(new Listener(listener));
     }
 
     public Integer dispatch(Action action) {
         this.state = this.reducer.apply(this.state, action);
-        for(Function<Integer, Integer> listener: listeners) {
+        for(Listener listener: listeners) {
             listener.apply(this.state);
         }
         return this.state;
     }
+
+    private static class Listener {
+        private Function<Integer, Integer> listener;
+
+        public Listener(Function<Integer, Integer> listener) {
+            this.listener = listener;
+        }
+
+        public void apply(Integer state) {
+            this.listener.apply(state);
+        }
+    }
 }
+
